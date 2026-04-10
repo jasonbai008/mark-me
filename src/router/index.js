b305f3db4c1dd2c3eff35c60106d77749947e6e0
JasonBai008 <byygkcg@163.com>
2026-04-10 09:47:21 +0800

    修改histor模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
-    history: createWebHashHistory(), // hash模式
-    // history: createWebHashHistory(), // history模式
+    // history: createWebHashHistory(), // hash模式
+    history: createWebHashHistory(), // history模式
     routes: [
         {
             path: '/',
